74649ba54eec49e33242551ff1a5bc62471432a9
minejo <jonathan.swjtu@gmail.com>
2017-04-03 20:03:51 +0800

    添加competition项目，实现竞赛的抓取

---
+++ competition.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# Created on 2017-03-28 22:14:07
+# Project: LecturePush
+
+from pyspider.libs.base_handler import *
+import re
+import datetime as dt
+from datetime import datetime,timedelta
+
+from pyspider.libs.base_handler import *
+import pymysql
+from lxml import etree
+from tidylib import tidy_document
+import urllib2, cookielib, re, jieba
+
+conn = pymysql.connect(host='ip', user='root', passwd='password', db='db', charset="utf8")
+cursor = conn.cursor()
+
+limdate = dt.datetime.now() + dt.timedelta(days=-60) # 日期下限
+
+competition_list = [
+    '全国大学生ACM程序设计大赛',
+    '全国大学生广告艺术大赛',
+    '全国大学生智能车竞赛',
+    '全国大学生交通科技竞赛',
+    '全国大学生物流设计大赛',
+    '全国大学生电子设计竞赛',
+    '全国大学生周培源力学竞赛',
+    '全国大学生工程训练综合能力竞赛',
+    '全国大学生机器人大赛',
+    '全国大学生数学建模竞赛',
+    '中国大学生互联网创新创业大赛',
+    '全国大学生机械创新设计大赛',
+    '全国大学生节能减排社会实践与科技竞赛',
+    '全国大学生结构设计竞赛',
+    '大学生生命之星科技邀请赛',
+    '全国大学生物理实验竞赛大学生物理学术竞赛',
+    '大学生英语系列挑战赛',
+    '创青春大学生课外学术科技作品竞赛与创业计划大赛',
+    '中国大学生服务外包创新创业大赛'
+]
+competition_split = []
+
+
+
+class Handler(BaseHandler):
+    
+    def __init__(self):
+        self.conn = conn
+        self.cursor = cursor
+        sql = '''
+            CREATE TABLE IF NOT EXISTS Competition(
+              id INT PRIMARY KEY AUTO_INCREMENT,
+              title VARCHAR(100),
+              publishdate datetime,
+              detail TEXT
+        )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;'''
+        self.cursor.execute(sql)
+        self.conn.commit()
+        
+        for match in competition_list:
+            splitword = jieba.lcut_for_search(match) #中文分词
+            onesplit = []
+            for word in splitword:
+                if re.match('全国|大学|学生|大学生|学院|竞赛|大赛|与|挑战|作品|计划'.decode('utf8'), word) == None:
+                    onesplit.append(word)
+            competition_split.append(onesplit)
+        
+    crawl_config = {
+        'headers': {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, sdch",
+            "Accept-Language": "zh-CN,zh;q=0.8",
+            "Cache-Control": "max-age=0",
+            "Connection": "keep-alive",
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36', }
+    }
+
+
+
+    @every(minutes=3 * 60)
+    def on_start(self):
+        self.crawl('http://dean.swjtu.edu.cn/servlet/NewsAction?Action=NewsMore',  callback=self.index_page, validate_cert=False, auto_recrawl=True)
+
+
+    '''
+    工具方法－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
+    '''
+
+    #删除不必要的字符
+    def deletetabnenter(self, string):
+        # print 'deletetabnenter\t'
+        newstring = ''
+        for char in string:
+            if re.match('[ \n\t“”"、+（）()]'.decode('utf8'), char) == None and char != u'\xa0':
+                newstring += char
+        return newstring
+
+
+
+    def close_crawl(self):
+        pass
+
+    def iscompetition(self, title):
+        flag = 0
+        for cursor in range(len(competition_split)):
+            num = 0
+            for word in competition_split[cursor]:
+                if title.find(word) != -1:
+                    num += 1
+            if num > 1:
+                flag = 1
+                break
+        if flag == 1:
+            return True
+        else:
+            return False
+
+    '''
+    页面处理方法
+    '''
+    def index_page(self, response):
+        newslist = response.doc('ul.IndexMenu li').items()
+        for new in newslist:
+            if new.text() != "":
+                title = self.deletetabnenter(new("div a").text())
+                date = new('[style="float:left;width:75px; text-align:center; overflow:hidden;"]').text()
+                url = new("div a").attr.href
+                publishdate = datetime.strptime(date, '%Y-%m-%d')
+                if publishdate < limdate:
+                    break
+                if re.search('奖|绩|获|记|名单|课|结果|简报|中学生'.decode('utf8'), title) == None and title.find('赛'.decode('utf8')) != -1:
+                    if self.iscompetition(title):
+                        self.crawl(url, callback=self.dean_competition_detail_page, save={'publishdate': publishdate})
+
+        pagejs = response.doc('script')
+        all_pages = re.findall('var allPage = "(.*?)"', pagejs.text(), re.S)[0]
+        current_pages = re.findall('var page = "(.*?)"', pagejs.text(), re.S)[0]
+        if int(all_pages) > 1:            
+            for page in range(2, int(all_pages) + 1):
+                page_link = 'http://dean.swjtu.edu.cn/servlet/NewsAction?Action=NewsMore&page={}'.format(
+                    page)
+                if page < 5:
+                    self.crawl(page_link, callback=self.index_page)
+
+    @config(priority=2)
+    def dean_competition_detail_page(self, response):
+        title= response.doc('font[size="5"]').text()
+        detail = response.doc('td[style="line-height: 150%"]').html()
+        publishdate = response.save['publishdate']
+        if datetime.strptime(publishdate , '%Y-%m-%d %H:%M:%S') >= limdate:
+            return  {
+                "title": title,
+                "publishdate": datetime.strptime(publishdate, '%Y-%m-%d %H:%M:%S'),
+                "detail": detail,
+            }
+
+
+    def on_result(self, result):
+        if not result:
+            return
+        super(Handler, self).on_result(result)
+        title = result['title']
+        publishdate = result['publishdate']
+        detail = result['detail']
+        self.cursor.execute('SELECT 1 FROM Competition WHERE title=%s LIMIT 1', title)  # 插入前判断是否存在
+        if len(cursor.fetchall()) == 0:
+            self.cursor.execute(
+                'INSERT INTO Competition(title, publishdate, detail) VALUES (%s, %s, %s)',
+                (title, publishdate, detail))
+            self.conn.commit()
+            print('insert success!')
+        else:
+            print('data has already exists.')
+
--- lecture.py
+++ lecture.py
@@ -9,40 +9,8 @@ from datetime import datetime
 from pyspider.libs.base_handler import *
 import pymysql
 
-conn = pymysql.connect(host='ip', user='root', passwd='password', db='db',
-                       charset="utf8")
+conn = pymysql.connect(host='ip', user='root', passwd='password', db='db', charset="utf8")
 cursor = conn.cursor()
-from lxml import etree
-from tidylib import tidy_document
-import urllib2,cookielib,re,jieba
-
-
-limdate = 20160900#日期下限
-
-
-def iopen(url):
-  cookie = cookielib.CookieJar()
-  handler = urllib2.HTTPCookieProcessor(cookie)
-  opener = urllib2.build_opener(handler)
-  # 将cookies绑定到一个opener cookie由cookielib自动管理
-  header = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:51.0) Gecko/20100101 Firefox/51.0'
-  }
-  request = urllib2.Request(url,'',header)
-  # 构造request请求
-  html = opener.open(request).read().decode('utf8')
-  return html
-def deletetabnenter(string):
-  #print 'deletetabnenter\t'
-  newstring = ''
-  for char in string:
-    if re.match('[ \n\t“”"、+（）()]'.decode('utf8'),char) == None and char != u'\xa0':
-      newstring += char
-  return newstring
 
 class Handler(BaseHandler):
     crawl_config = {
@@ -69,13 +37,6 @@ class Handler(BaseHandler):
               speakerbrif TEXT,
               detail TEXT
         )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;'''
-        sql = '''
-            CREATE TABLE IF NOT EXISTS Matches(
-              id INT PRIMARY KEY AUTO_INCREMENT,
-              title VARCHAR(100),
-              matchtime datetime,
-              detail TEXT
-        )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;'''
         self.cursor.execute(sql)
         self.conn.commit()
 
@@ -86,16 +47,16 @@ class Handler(BaseHandler):
             callback=self.swjtu_page, validate_cert=False, auto_recrawl=True)
         self.crawl('http://dean.swjtu.edu.cn/servlet/LectureAction?Action=LectureMore&SelectType=Month',
                    callback=self.dean_page, validate_cert=False, auto_recrawl=True)
-        #match(self)
 
     def swjtu_page(self, response):
         for each in response.doc('[cmsid="34403899"] a').items():
             self.crawl(each.attr.href, callback=self.swjtu_detail_page)
         currentpage = response.doc('div.page_blue span.current').text()
-        if not currentpage and int(currentpage) <= 7:
-            for each in response.doc('.page_blue a').items():
-                if (each.text() == '>>'):
-                    self.crawl(each.attr.href, callback=self.swjtu_page)
+        if not currentpage == "":
+            if int(currentpage) <= 7:
+                for each in response.doc('.page_blue a').items():
+                    if (each.text() == '>>'):
+                        self.crawl(each.attr.href, callback=self.swjtu_page)
 
     def dean_page(self, response):
         html = response.doc('script')
@@ -107,78 +68,9 @@ class Handler(BaseHandler):
                 lecture_page_link = 'http://dean.swjtu.edu.cn/servlet/LectureAction?Action=LectureMore&SelectType=Month&page={}'.format(
                     item)
                 self.crawl(lecture_page_link, callback=self.dean_page)
-                
-    def match(self):
-        a = etree.HTML(tidy_document(iopen('http://dean.swjtu.edu.cn/servlet/NewsView?NewsID=BEBF684C17BED733'))[0])
-        b = etree.HTML(tidy_document(iopen('http://dean.swjtu.edu.cn/servlet/NewsView?NewsID=0C618242FA8B1810'))[0])
-        tree = etree.HTML('<html></html>')
-        tree.append(a)
-        tree.append(b)
-        tdlist = tree.xpath('/html/html/body/div/table/tr/td/div/table/tbody/tr/td[@width="286"]')[1:]
-        textcluster = []
-        for td in tdlist:
-            textcluster.append(td.xpath('.//text()'))
-        tmp = []
-        for textlist in textcluster:
-            name = ''
-            for text in textlist:
-                name += text
-            if name.find('赛'.decode('utf8')) != -1:
-                tmp.append(deletetabnenter(name))
-        textlist = tmp
-        textlist2 = textlist
-        textcluster = []
-        for text in textlist:
-            tmp = jieba.lcut_for_search(text)
-            tttmp = []
-            for ttmp in tmp:
-                if re.match('全国|大学|学生|大学生|竞赛|大赛|学院|创|与|挑战|作品|计划'.decode('utf8'),ttmp) == None:
-                    tttmp.append(ttmp)
-            textcluster.append(tttmp)
-        url = 'http://dean.swjtu.edu.cn/servlet/NewsAction?Action=NewsMore&page='
-        anchorlist = []
-        page = 1
-        while 1:
-            html = tidy_document(iopen(url+str(page)))[0]
-            tree = etree.HTML(html)
-            lilist = tree.xpath('/html/body/div/div/ul/li')
-            for li in lilist:
-                name = deletetabnenter(li.xpath('./div/a/text()')[0])
-                date = deletetabnenter(li.xpath('./div/text()')[2])
-                url2 = deletetabnenter('http://dean.swjtu.edu.cn'+li.xpath('./div/a/@href')[0][2:])
-                date = int(date[:4]+date[5:7]+date[8:])
-                if date < limdate:
-                    break
-                if re.search('奖|绩|获|记|名单|课|结果|简报|中学生'.decode('utf8'),name) == None and name.find('赛'.decode('utf8')) != -1:
-                    anchorlist.append([name,date,url2])
-            if date < limdate:
-                break
-            page += 1
-        _ = []
-        for cursor in range(len(textlist2)):
-            tmp = []
-            for anchor in anchorlist:
-                num = 0
-                for text in textcluster[cursor]:
-                    if anchor[0].find(text) != -1:
-                        num += 1
-                tmp.append(num)
-            if max(tmp) > 1:
-                for cs in range(len(tmp)):
-                    if tmp[cs] > 1:
-                        if _.count(anchorlist[cs]) == 0:
-                            _.append(anchorlist[cs])
-        for cursor in range(len(_)-1):
-            for cs in range(len(_)-1):
-                if _[cs][1] < _[cs+1][1]:
-                    tmp = _[cs+1]
-                    _[cs+1] = _[cs]
-                    _[cs] = tmp
-        for __ in _:
-            self.crawl(__[2], callback=self.match_detail_page)
 
     def handleTime(self, text):
-        text = text.split('-')[0]
+        text = text.split('---')[0]
         timenum = re.sub("\D", "", text)
         time = datetime.strptime(timenum, '%Y%m%d%H%M')
         return time
@@ -207,7 +99,7 @@ class Handler(BaseHandler):
         time = response.doc(
             '[style="width:50px; height:80px; margin:10px auto; text-align:center; font-size:14px; font-weight:bold; line-height:23px; color:#6c6c6c;"]').text()
         time = self.handleTime(date + " " + time)
-        detail = response.doc('[style="width:700px; height:auto; margin:0 auto;"]').text()
+        detail = response.doc('[style="width:700px; height:auto; margin:0 auto;"]').html()
 
         deadtime = self.deadTime(datetime.now())
         if time >= deadtime:
@@ -228,7 +120,7 @@ class Handler(BaseHandler):
         time = datetime.now()
         place = ""
         speaker = ""
-        detail = response.doc('table#table1 tr td').text()
+        detail = response.doc('table#table1 tr td').html()
         for each in response.doc('table#table1 tr td').items():
             if line == 0:
                 title = each.text()
@@ -252,20 +144,6 @@ class Handler(BaseHandler):
                 "place": place,
             }
 
-    @config(priority=2)
-    def match_detail_page(self, response):
-        title = ""
-        line = 0
-        time = datetime.now()
-        detail = 
-        deadtime = self.deadTime(datetime.now())
-        if time >= deadtime:
-            return {
-                "title": title,
-                "time": time.strftime('%Y-%m-%d %H:%M'),
-                "detail": detail
-            }
-
     def on_result(self, result):
         if not result:
             return
@@ -276,9 +154,12 @@ class Handler(BaseHandler):
         speaker = result['speaker']
         speakerbrif = result['speakerbrif']
         detail = result['detail']
-
-        self.cursor.execute(
+        self.cursor.execute('SELECT 1 FROM Lecture WHERE title=%s LIMIT 1', title) #插入前判断是否存在
+        if len(cursor.fetchall()) == 0:
+            self.cursor.execute(
             'INSERT INTO Lecture(title, lecturetime, place, speaker, speakerbrif, detail) VALUES (%s, %s, %s, %s, %s, %s)',
             (title, time, place, speaker, speakerbrif, detail))
-        self.conn.commit()
-        print('insert success!')
+            self.conn.commit()
+            print('insert success!')
+        else:
+            print('data has already exists.')
